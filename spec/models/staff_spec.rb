7ccd5de442a6a2f684b6f9e77851179c4b096f7b
toyman640 <falakoadebayor@gmail.com>
2024-04-09 12:35:18 +0100

    Test staff model

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Staff, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not save user without data' do
+    staff = Staff.new
+    expect(staff.valid?).to eq false
+  end
+
+  it 'should save user with valid attributes' do
+    staff = Staff.new(username: 'toyman', password: 'toyman123',
+                    password_confirmation: 'toyman123', role: 'doctor')
+    expect(staff.valid?).to eq true
+  end
 end

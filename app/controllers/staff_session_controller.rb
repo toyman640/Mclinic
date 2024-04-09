ec53def387e0580b70e368a47dfed6e58a1bc44a
toyman640 <falakoadebayor@gmail.com>
2024-04-09 13:35:41 +0100

    Modify session method

--- app/controllers/staff_session_controller.rb
+++ app/controllers/staff_session_controller.rb
@@ -8,6 +8,11 @@ class StaffSessionController < ApplicationController
 
     if @staff && @staff.authenticate(params[:user][:password])
       session[:staff_id] = @staff.id
+      if @staff.role == 'doctor'
+        redirect_to doctor_dashboard_path
+      elsif @staff.role == 'receptionist'
+        redirect_to receptionist_dashboard_path
+      end
       redirect_to root_path
     else
       flash[:alert] = "Login failed"

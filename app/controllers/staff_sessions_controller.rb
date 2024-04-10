928e60c253a3fb04352d5a5613589c92d37bdccf
toyman640 <falakoadebayor@gmail.com>
2024-04-10 11:22:09 +0100

    Fix linters errors

--- app/controllers/staff_sessions_controller.rb
+++ app/controllers/staff_sessions_controller.rb
@@ -9,15 +9,15 @@ class StaffSessionsController < ApplicationController
 
     if @staff&.authenticate(params[:staff][:password])
       session[:staff_id] = @staff.id
-      if @staff.role == 'doctor'
-        redirect_to patients_path
-        puts 'Login successful: Good'
-      elsif @staff.role == 'receptionist'
-        redirect_to patients_path
-        puts 'Login successful: Good'
-      else
-        redirect_to root_path
-      end
+      # if @staff.role == 'doctor'
+      #   redirect_to patients_path
+      # elsif @staff.role == 'receptionist'
+      #   redirect_to patients_path
+      #   puts 'Login successful: Good'
+      # else
+      #   redirect_to root_path
+      # end
+      redirect_to patients_path
     else
       flash[:alert] = 'Login failed'
       puts 'Login failed: Bad'
